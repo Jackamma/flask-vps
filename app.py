5402f6e5ffb509a2cd7ce65a8470d38f33cdcc22
Jackamma <Jackamma@users.noreply.github.com>
2021-04-16 01:25:52 +0200

    Added ippodromo page and base.html

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def home():
 	return render_template('index.html')
 
-@app.route("/test/")
-def testPage():
-	return "This is a test2"
+@app.route("/ippodromo/")
+def ippo():
+	# Aggiungere verifica hash telegram
+	first_name = request.args.get('first_name')
+	return render_template('ippo.html', first_name=first_name)
 
 if __name__ == "__main__":
 	app.run(debug=True)
